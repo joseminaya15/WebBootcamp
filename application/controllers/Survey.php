a2292eb5db8ed285e84080914e07497bd31e337b
jhonataniberico <jhonatanibericom@gmail.com>
2018-01-31 16:19:15 -0500

    cambios en los controladores

--- application/controllers/Survey.php
+++ application/controllers/Survey.php
@@ -3,8 +3,34 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Survey extends CI_Controller {
 
+	function __construct() {
+        parent::__construct();
+        $this->load->model('M_encuesta');
+        $this->load->helper("url");//BORRAR CACHÉ DE LA PÁGINA
+        $this->output->set_header('Last-Modified:'.gmdate('D, d M Y H:i:s').'GMT');
+        $this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate');
+        $this->output->set_header('Cache-Control: post-check=0, pre-check=0',false);
+        $this->output->set_header('Pragma: no-cache');
+    }
+
 	public function index()
 	{
 		$this->load->view('v_survey');
+	}
+
+	function enviarEncuesta() {
+		$data['error'] = EXIT_ERROR;
+        $data['msj']   = null;
+        try {
+            $valor 		 = $this->input->post('valor');
+            $suggestions = $this->input->post('suggestions');
+            $future 	 = $this->input->post('future');
+            $rate 		 = $this->input->post('rate');
+            //$datos 		 = $this->M_encuesta->getDatosPersona($this->session->userdata('correo'));
+            $data['error'] = EXIT_SUCCESS;
+        } catch (Exception $e) {
+            $data['msj'] = $e->getMessage();
+        }
+        echo json_encode($data);
 	}
 }
--- public/js/index.js
+++ public/js/index.js
@@ -29,13 +29,47 @@ function enviarEncuesta(){
 	$("input:checked").each(function() {
 	  valor.push($(this).val());
 	});
-	var rate = $('#rate').val();
 	var suggestions = $('#suggestions').val();
-	var future = $('#future').val();
-	var rate = $(".button-select").text();
-	$($(':checkbox').prop('checked', true)).each(function() {
-	  console.log($(this));
+	var future 		= $('#future').val();
+	var rate 		= $(".button-select").find("span").text();
+	if(valor.length < 9){ 
+		return;
+	}
+	if(suggestions == null) {
+		return;
+	}
+	if(future == null) {
+		return;
+	}
+	if(rate == null) {
+		return;
+	}
+	$.ajax({
+		data  : { valor 	  : valor,
+				  suggestions : suggestions,
+				  future 	  : future,
+				  rate 		  : rate},
+		url   : 'Survey/enviarEncuesta',
+		type  : 'POST'
+	}).done(function(data){
+		try{
+        	data = JSON.parse(data);
+        	if(data.error == 0){
+				/*addLoading('idEncuesta');
+					setTimeout(function(){ 
+						stopLoading('idEncuesta')
+					}, 4000);*/
+        	}else {
+        		return;
+        	}
+      } catch (err){
+        msj('error',err.message);
+      }
 	});
+	/*$($(':checkbox').prop('checked', true)).each(function() {
+	  console.log($(this));
+	});*/
+
 }
 
 function numberRange(id){
