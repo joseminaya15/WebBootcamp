72c0383211cba5b9e4d312669d5cddf0b51465ee
joseminaya15 <jose.minayac15@gmail.com>
2018-02-01 10:32:57 -0500

    vistas title

--- application/views/v_survey.php
+++ application/views/v_survey.php
@@ -4,13 +4,13 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible"  content="IE=edge">
         <meta name="viewport"               content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
-        <meta name="description"            content="Proyecto de desarrollo de un producto wizard online que tenga un quiz y con una unica solucion que es SAP Business One">
-        <meta name="keywords"               content="SAP,producto wizard">
+        <meta name="description"            content="HPE Latin America Hybrid IT Bootcamp">
+        <meta name="keywords"               content="HPE Latin America Hybrid IT Bootcamp">
         <meta name="robots"                 content="Index,Follow">
         <meta name="date"                   content="January 25, 2018"/>
         <meta name="language"               content="es">
         <meta name="theme-color"            content="#000000">
-        <title>Encuestas Merino</title>
+        <title>HPE Latin America Hybrid IT Bootcamp</title>
         <link rel="shortcut icon" href="<?php echo RUTA_IMG?>logo/favicon.png">
         <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>bootstrap/bootstrap.min.css?v=<?php echo time();?>">
         <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>bootstrap-select/css/bootstrap-select.min.css?v=<?php echo time();?>">
